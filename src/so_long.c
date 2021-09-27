cd7530be0d30e8348b31e404831c47817c16be6e
Stannis Rupert <srupert@ev-q2.kzn.21-school.ru>
2021-09-27 20:48:20 +0300

    norminet

--- src/so_long.c
+++ src/so_long.c
@@ -1,15 +1,15 @@
 #include "ft_so_long.h"
 
-static void ft_check_params(int argc, char **argv);
+static void	ft_check_params(int argc, char **argv);
 
-int main (int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	ft_check_params(argc, argv);
 	ft_sl_run(*(argv + 1));
 	return (0);
 }
 
-static void ft_check_params(int argc, char **argv)
+static void	ft_check_params(int argc, char **argv)
 {
 	if (argc != 2 || argv == NULL)
 	{
