9f640c19d65de52f5c59da0888627ddf18c5a9fc
Stannis Rupert <srupert@ev-p4.kzn.21-school.ru>
2021-09-29 20:50:27 +0300

    new file

---
+++ src/ft_sl_inits.c
@@ -0,0 +1,8 @@
+#include "ft_so_long.h"
+
+void	ft_sl_init_res(t_res **res)
+{
+	*res = malloc(sizeof(t_res));
+	(*res)->mlx = mlx_init();
+	(*res)->win = mlx_new_window((*res)->mlx, 1920, 1080, "so_long");
+}
