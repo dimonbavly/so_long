33e772ebd6127fd01f44203ee9bca507298f1b08
Stannis Rupert <srupert@ev-q2.kzn.21-school.ru>
2021-09-30 11:48:40 +0300

    refactor

--- src/ft_sl_movement.c
+++ src/ft_sl_movement.c
@@ -6,31 +6,20 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 11:40:46 by                   #+#    #+#             */
-/*   Updated: 2021/09/30 11:40:46 by                  ###   ########.fr       */
+/*   Updated: 2021/09/30 11:40:48 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_so_long.h"
 
 void	ft_sl_move(int key, t_res *res)
 {
+	res->map->content[res->map->y_h][res->map->x_w] = '0';
 	if (key == 13)
-	{
-		res->map->content[res->map->y_h][res->map->x_w] = '0';
 		res->map->content[--(res->map->y_h)][res->map->x_w] = 'P';
-	}
 	else if (key == 0)
-	{
-		res->map->content[res->map->y_h][res->map->x_w] = '0';
 		res->map->content[res->map->y_h][--(res->map->x_w)] = 'P';
-	}
 	else if (key == 1)
-	{
-		res->map->content[res->map->y_h][res->map->x_w] = '0';
 		res->map->content[++(res->map->y_h)][res->map->x_w] = 'P';
-	}
 	else if (key == 2)
-	{
-		res->map->content[res->map->y_h][res->map->x_w] = '0';
 		res->map->content[res->map->y_h][++(res->map->x_w)] = 'P';
-	}
 }
