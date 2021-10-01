0dc793165619cccbe257f30a41cf5bdcae18d2d0
Stannis Rupert <srupert@ev-d4.kzn.21-school.ru>
2021-10-01 08:45:44 +0300

    refactor

--- src/ft_sl_movement.c
+++ src/ft_sl_movement.c
@@ -6,24 +6,63 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 11:40:46 by                   #+#    #+#             */
-/*   Updated: 2021/09/30 22:30:54 by                  ###   ########.fr       */
+/*   Updated: 2021/10/01 08:44:42 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_so_long.h"
 
-int	ft_sl_check_movement(int key, t_res *res);
-void	ft_sl_check_tile(t_res *res, char c);
+int		*ft_sl_check_tile(t_res *res, char c);
+int		*ft_sl_move_pre(int key);
 
 void	ft_sl_move(int key, t_res *res)
+{
+	int	*vec;
+	int	*ret;
+
+	vec = ft_sl_move_pre(key);
+	if (res->map->content[res->gdata->y_h + vec[0]][res->gdata->x_w + vec[1]] \
+	!= '1')
+	{
+		ret = ft_sl_check_tile(res, res->map->content \
+		[res->gdata->y_h + vec[0]][res->gdata->x_w + vec[1]]);
+		if (res->gdata->things == 0)
+			return ;
+		res->map->content[res->gdata->y_h][res->gdata->x_w] = '0';
+		res->gdata->x_w += vec[1];
+		res->gdata->y_h += vec[0];
+		res->map->content[res->gdata->y_h][res->gdata->x_w] = 'P';
+		res->gdata->things += ret[0];
+		res->gdata->end_game += ret[1];
+		(res->gdata->way)++;
+	}
+}
+
+int	*ft_sl_check_tile(t_res *res, char c)
+{
+	int	thing_vec;
+	int	end_game_vec;
+
+	thing_vec = 0;
+	end_game_vec = 0;
+	if (c == 'C')
+		thing_vec = -1;
+	else if (c == 'E')
+	{
+		if (res->gdata->things == 0)
+			end_game_vec = 1;
+	}
+	return ((int []){thing_vec, end_game_vec});
+}
+
+int	*ft_sl_move_pre(int key)
 {
 	int		*keys;
 	int		**vec;
 	int		i;
 
-	if (res->gdata->end_game)
-		return ;
 	keys = (int []){13, 0, 1, 2};
-	vec = (int *[]){(int []){-1, 0}, (int []){0, -1}, (int []){1, 0}, (int []){0, 1}};
+	vec = (int *[]){(int []){-1, 0}, (int []){0, -1}, \
+	(int []){1, 0}, (int []){0, 1}};
 	i = 0;
 	while (i < 4)
 	{
@@ -31,46 +70,5 @@ void	ft_sl_move(int key, t_res *res)
 			break ;
 		i++;
 	}
-	if (res->map->content[res->gdata->y_h + vec[i][0]][res->gdata->x_w + vec[i][1]] != '1')
-	{
-		ft_sl_check_tile(res, res->map->content[res->gdata->y_h + vec[i][0]][res->gdata->x_w + vec[i][1]]);
-		if (res->gdata->things == 0)
-			return ;
-		res->map->content[res->gdata->y_h][res->gdata->x_w] = '0';
-		res->gdata->x_w += vec[i][1];
-		res->gdata->y_h += vec[i][0];
-		res->map->content[res->gdata->y_h][res->gdata->x_w] = 'P';
-		(res->gdata->way)++;
-	}
+	return (vec[i]);
 }
-
-//todo rudiment
-int	ft_sl_check_movement(int key, t_res *res)
-{
-	char	c;
-
-	if (key == 13)
-		c = res->map->content[res->gdata->y_h - 1][res->gdata->x_w];
-	else if (key == 0)
-		c = res->map->content[res->gdata->y_h][res->gdata->x_w - 1];
-	else if (key == 1)
-		c = res->map->content[res->gdata->y_h + 1][res->gdata->x_w];
-	else
-		c = res->map->content[res->gdata->y_h][res->gdata->x_w + 1];
-	if (c == '1')
-		return (0);
-	else
-		return (1);
-}
-
-void	ft_sl_check_tile(t_res *res, char c)
-{
-	if (c == 'C')
-		(res->gdata->things)--;// todo fix this - goes to -1
-	else if (c == 'E')
-	{
-		if (res->gdata->things == 0)
-			res->gdata->end_game = 1;
-	}
-}
-
