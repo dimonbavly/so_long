5f9e1817a29cc552a38a6aa596c587635cbd093a
Stannis Rupert <srupert@ev-d4.kzn.21-school.ru>
2021-10-01 09:36:43 +0300

    fix logic - if hero move to exit tile

--- src/ft_sl_movement.c
+++ src/ft_sl_movement.c
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 11:40:46 by                   #+#    #+#             */
-/*   Updated: 2021/10/01 09:05:23 by                  ###   ########.fr       */
+/*   Updated: 2021/10/01 09:35:12 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_so_long.h"
@@ -23,7 +23,8 @@ void	ft_sl_move(int key, t_res *res)
 		return ;
 	vec = ft_sl_move_pre(key);
 	if (res->map->content[res->gdata->y_h + vec[0]][res->gdata->x_w + vec[1]] \
-	!= '1')
+	!= '1' && (res->map->content[res->gdata->y_h + vec[0]] \
+	[res->gdata->x_w + vec[1]] != 'E' && res->gdata->things > 0 ))
 	{
 		ret = ft_sl_check_tile(res, res->map->content \
 		[res->gdata->y_h + vec[0]][res->gdata->x_w + vec[1]]);
