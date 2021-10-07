501a650bff237a678753e0062ced59ca60e8c467
Stannis Rupert <srupert@ev-p4.kzn.21-school.ru>
2021-10-07 09:48:04 +0300

    add check null pointer function

--- src/error_handler.c
+++ src/error_handler.c
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/02 11:04:17 by                   #+#    #+#             */
-/*   Updated: 2021/10/02 11:04:17 by                  ###   ########.fr       */
+/*   Updated: 2021/10/07 09:40:56 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_so_long.h"
@@ -15,4 +15,12 @@ void	error_n_xit(char *str, int num)
 {
 	printf("Error\n>>>%s\n", str);
 	exit (num);
+}
+
+void	*check_null_ptr(void *ptr)
+{
+	if (ptr == NULL)
+		error_n_xit(strerror(errno), EXIT_FAILURE);
+	else
+		return (ptr);
 }
